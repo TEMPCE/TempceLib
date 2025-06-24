0e27eb126a3df510769ec1f08468dd13351a8117
MAIOTA <h20051007@gmail.com>
2025-06-24 13:35:24 +0900

    仕様再定義のため一度削除

--- src/main/java/com/Tempce/tempceLib/TempceLib.java
+++ src/main/java/com/Tempce/tempceLib/TempceLib.java
@@ -1,27 +1,15 @@
 package com.Tempce.tempceLib;
 
-import com.Tempce.tempceLib.message.MessageManager;
-import com.Tempce.tempceLib.message.TranslationManager;
-import com.Tempce.tempceLib.message.examples.MessageExampleCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TempceLib extends JavaPlugin {
 
   private static TempceLib instance;
-  private MessageManager messageManager;
-  private TranslationManager translationManager;
 
   @Override
   public void onEnable() {
     // Plugin startup logic
     instance = this;
-    
-    // Initialize managers
-    this.messageManager = new MessageManager();
-    this.translationManager = new TranslationManager();
-    
-    // Register example command
-    getCommand("tempce").setExecutor(new MessageExampleCommand(this));
     
     getLogger().info("TempceLib has been enabled!");
     getLogger().info("Message system initialized with translation support");
@@ -36,21 +24,5 @@ public final class TempceLib extends JavaPlugin {
 
   public static TempceLib getInstance() {
     return instance;
-  }
-  
-  /**
-   * MessageManagerのインスタンスを取得
-   * @return MessageManager
-   */
-  public MessageManager getMessageManager() {
-    return messageManager;
-  }
-  
-  /**
-   * TranslationManagerのインスタンスを取得
-   * @return TranslationManager
-   */
-  public TranslationManager getTranslationManager() {
-    return translationManager;
   }
 }
